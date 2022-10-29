e409d2c18c6cc197f0a0d2aed84c5667f3843440
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-10-29 10:31:29 +0530

    Initial Commit

---
+++ demos/parser._isCJSCodeBase.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Package: 
+ * Author: Ganesh B
+ * Description: JS functions to check all imports or required modules and check if the file is a ES Module or a CJS/ JS Module or Script
+ * Install: npm i require-urls --save
+ * Github: https://github.com/ganeshkbhat/get-imports
+ * npmjs Link: 
+ * File: demos/parser.demo.require.cache.test.file.cjs
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
